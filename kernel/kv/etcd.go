c98947c39cc96820a99893b16c682fa66914cddc
Ikchan Kwon <kighie@rhizomata.io>
2020-01-13 00:12:43 +0900

    etcd watch stream problem

--- kernel/kv/etcd.go
+++ kernel/kv/etcd.go
@@ -46,6 +46,8 @@ func (watcher *Watcher) start() {
 		}
 		eventType := ParseType(int32(watchResp.Events[0].Type))
 		watcher.handler(eventType, key, rowID, kv.Value)
+		// etcd watch stream problem
+		time.Sleep(2 * time.Millisecond)
 	}
 }
 
